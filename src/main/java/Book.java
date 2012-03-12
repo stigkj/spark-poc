566c882f7a1ac77ff700351d02573aab5a5a97b7
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2012-03-12 15:17:18 +0100

    First commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
